f9955feeab317edab0848af3d5f6f44119e33036
XanderWonder <macluab@gmail.com>
2019-01-24 15:29:31 +0000

    commit

--- src/main/scala/Movies.scala
+++ src/main/scala/Movies.scala
@@ -3,11 +3,13 @@ import org.apache.log4j._
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
 import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 
 object Movies extends App {
   Logger.getLogger("org").setLevel(Level.ERROR)
   val conf = new SparkConf().set("spark.driver.allowMultipleContexts", "true").setAppName("test").setMaster("local[*]")
 
+  def ddouble(A:Int):Int={A * A}
   val sc = new SparkContext(conf)
   val rddI = sc.textFile("C:\\Users\\Admin\\Desktop\\Scala\\movies.txt")
 
@@ -24,6 +26,9 @@ object Movies extends App {
       StructField("movieRating", IntegerType, true) ::
       StructField("timeStamp", LongType, true) :: Nil)
 
+  val abc = udf(ddouble(_),IntegerType)
+  sqlContext.udf.register("abc",ddouble(_))
+
   val rdd2 = MovieFilter.map(x => Row(x._1, x._2, x._3, x._4))
 
   val df2= sqlContext.createDataFrame(rdd2,schema)
@@ -31,7 +36,8 @@ object Movies extends App {
   var df3 =df2.filter(df2("movieRating")=== 5).groupBy("movieId").count
 
   df2.registerTempTable("testTable")
-  sqlContext.sql("SELECT movieId,watchAmount FROM testTable WHERE watchAmount > 200 LIMIT 20").show()
+  sqlContext.sql("SELECT movieId as MovieID ,watchAmount as WatchAmount, abc(watchAmount) as Multiplier FROM testTable WHERE watchAmount > 200 LIMIT 20").show()
+
 
 //  var highest_rated = df3.orderBy(df3("count").desc).first()
 //  print(highest_rated)
